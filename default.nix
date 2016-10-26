9ba61268123a54d887ed04b7009a92f0d788457b
Manuel Reinhardt <reinhardt@syslab.com>
2016-10-26 07:56:49 +0100

    Fixed ssl in nix shell

--- default.nix
+++ default.nix
@@ -3,5 +3,10 @@ with import <nixpkgs> {}; {
     name = "octodon";
     buildInputs = [ stdenv python27Full python27Packages.virtualenv hamster-time-tracker libxml2 git subversion ];
     LIBRARY_PATH="${libxml2}/lib";
+    shellHook = ''
+      unset http_proxy
+      export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
+      export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
+    '';
   };
 }

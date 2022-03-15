c4550743d49fb4afa34246b26ef5609d8941df33
Manuel Reinhardt <reinhardt@syslab.com>
2022-03-15 07:23:06 +0100

    Github: More robust issue getter

--- src/octodon/github.py
+++ src/octodon/github.py
@@ -30,60 +30,74 @@ class Github(object):
 
     def __init__(self, token, organization, project_num):
         self._github = GitHubAPI(bearer_token=token)
-        query = """
-            query($organization:String!, $project_num:Int!) {
-                organization(login: $organization) {
-                    projectNext(number: $project_num) {
-                        items(first:60) {
-                        nodes {
-                            fieldValues(first:60) {
+        self.organization = organization
+        self.project_num = project_num
+
+    @property
+    def issues(self):
+        if not hasattr(self, "_issues"):
+            query = """
+                query($organization:String!, $project_num:Int!) {
+                    organization(login: $organization) {
+                        projectNext(number: $project_num) {
+                            items(first:60) {
                             nodes {
-                                value
-                                projectField {
+                                fieldValues(first:60) {
+                                nodes {
+                                    value
+                                    projectField {
+                                        name
+                                    }
+                                }
+                                }
+                                content {
+                                ...on Issue {
+                                    number
+                                    title
+                                    repository {
+                                    owner {
+                                        login
+                                    }
                                     name
+                                    }
+                                }
                                 }
                             }
                             }
-                            content {
-                            ...on Issue {
-                                number
-                                title
-                                repository {
-                                owner {
-                                    login
-                                }
-                                name
-                                }
-                            }
-                            }
-                        }
                         }
                     }
                 }
-            }
-        """
-        result = self._github.graphql(
-            query, {"organization": organization, "project_num": project_num}
-        )
-        nodes = result["data"]["organization"]["projectNext"]["items"]["nodes"]
-        self._issues = {}
-        for node in nodes:
-            contract = next(
-                (
-                    field["value"]
-                    for field in node["fieldValues"]["nodes"]
-                    if field["projectField"]["name"] == "Contracts"
-                ),
-                None,
+            """
+            result = self._github.graphql(
+                query,
+                {"organization": self.organization, "project_num": self.project_num},
             )
-            owner = node["content"]["repository"]["owner"]["login"]
-            name = node["content"]["repository"]["name"]
-            number = node["content"]["number"]
-            title = node["content"]["title"]
-            self._issues.setdefault(owner, {}).setdefault(name, {})[number] = {
-                "contract": contract,
-                "title": title,
-            }
+            nodes = result["data"]["organization"]["projectNext"]["items"]["nodes"]
+            self._issues = {}
+            for node in nodes:
+                contract = next(
+                    (
+                        field["value"]
+                        for field in node["fieldValues"]["nodes"]
+                        if field["projectField"]["name"] == "Contracts"
+                    ),
+                    None,
+                )
+                if not node.get("content"):
+                    # probably a draft card
+                    continue
+                repo = node["content"].get("repository")
+                if not repo:
+                    continue
+                owner = repo["owner"]["login"]
+                name = repo["name"]
+                number = node["content"]["number"]
+                title = node["content"]["title"]
+                self._issues.setdefault(owner, {}).setdefault(name, {})[number] = {
+                    "contract": contract,
+                    "title": title,
+                }
+        return self._issues
 
     def get_project_id(self, organization, project_num):
         query = """
@@ -96,11 +110,11 @@ class Github(object):
             }
         """
         variables = {
-            "organization": organization,
-            "project_num": project_num,
+            "organization": self.organization,
+            "project_num": self.project_num,
         }
         result = self._github.graphql(query, variables)
-        return result["organisation"]["projectNext"]["id"]
+        return result["organization"]["projectNext"]["id"]
 
     def get_issue(self, issue_no):
         match = self.ticket_pattern.match(issue_no)
@@ -108,6 +122,6 @@ class Github(object):
             return None
         org, repo, number = match.groups()[1:4]
         try:
-            return GithubIssue(self._issues[org][repo][int(number)])
+            return GithubIssue(self.issues[org][repo][int(number)])
         except KeyError:
             return None
